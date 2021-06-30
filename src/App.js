4cec8730ae70232762979cd1a4c1d831136e38b3
Tyler Farris <Tylerpfarris@gmail.com>
2021-06-30 11:02:03 -0600

    Components added to App and useFirestore hook created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 
+import React from 'react';
 import './App.css';
+import Form from './Form';
+import { useFirestore } from '../hooks/useFirestore';
+import Messages from './Messages';
+import Table from './Table';
 
 function App() {
-  return (
-    <div className="App">
 
+  const { loading, users, error, username, query, onSubmit, setQuery } = useFirestore();
+  
+return(
+    <div className="App">
+    <Form
+      onSubmit={onSubmit}
+      setQuery={setQuery} />
+
+    <Messages
+      loading={loading}
+      error={error}
+      username={username}
+      query={query} />
+
+    <Table
+      users={users} />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,69 @@
+  
+import { useEffect, useState } from "react";
+import { fetchUser } from "../utils/gitHubApi";
+import firebase from '../firebase';
+
+
+export const useFirestore = () => {
+
+   const [loading, setLoading] = useState(false)
+   const [users, setUsers] = useState([])
+   const [error, setError] = useState(0)
+   const [query, setQuery] = useState('')
+   const [username, setUsername] = useState(null);
+   
+   const usersRef = firebase.firestore().collection('users');
+
+    const onSubmit = async (e) => {
+    e.preventDefault();
+
+      fetchUser(query)
+        .then(data => {
+           console.log(data)
+          if (data.status >= 400) {
+            setError(2);
+            setUsername(data.login);
+          } else if (users.some(user => user.id === data.id)) {
+            setError(3);
+            setUsername(data.login);
+            return;
+          }
+
+          usersRef.doc()
+            .set(data)
+            .then(() => {
+              setError(1)
+              setUsername(data.login)
+            })
+            .catch((err) => {
+              console.error(err)
+            })
+        })
+    setTimeout(() => {
+        setError(0)
+      }, 3500)
+      
+      
+    }
+
+   useEffect(() => {
+      const usersRef = firebase.firestore().collection('users');
+       const getUsersFromFB = () => {
+         setLoading(true);
+         usersRef.onSnapshot((querySnapshot) => {
+            const usersDB = [];
+            querySnapshot.forEach((doc) => {
+               usersDB.push(doc.data());
+            });
+         setUsers(usersDB);
+         setLoading(false);
+   
+         })
+      }
+       getUsersFromFB();
+       return;
+   }, [])
+   
+   return { loading, users, error, username, query, onSubmit, setQuery, setUsers}
+   
+}
